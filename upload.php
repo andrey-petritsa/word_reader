8c5200f8f800554bf65a5bdd5d59861e023e4a0b
root <root@andrey.fvds.ru>
2020-08-01 03:02:13 -0400

    Changed upload diretory path

--- upload.php
+++ upload.php
@@ -15,6 +15,7 @@ if (isset($_FILES['doc_file']) && $_FILES['doc_file']['error'] === UPLOAD_ERR_OK
     if($file_manager->isFileValid($file_extension)) { // Если файл поддерживается программой
         $uploaded_file_path = Settings::$directory_path . "/" . $file_manager->generateRandomFileName($file_extension);
         $saved_file_path = $file_manager->saveFile($file_from_user_temp_path, $uploaded_file_path, $file_extension);
+	//$python_command = sprintf(__DIR__ . "/" . 'word_reader.py %s', $uploaded_file_path . " 2>&1");
         $python_command = escapeshellcmd(sprintf(__DIR__ . "/" . 'word_reader.py %s', $uploaded_file_path));
         $script_output = shell_exec($python_command);
         if(is_numeric($script_output)) {
